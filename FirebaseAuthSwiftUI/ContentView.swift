eb61a8ec7acfd350eb04fa193ff8adede33bbb13
Giuseppe <Sapienza>
2020-06-06 13:50:19 +1000

    Initial Commit

---
+++ FirebaseAuthSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FirebaseAuthSwiftUI
+//
+//  Created by Giuseppe Sapienza on 6/6/20.
+//  Copyright © 2020 Giuseppe Sapienza. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
